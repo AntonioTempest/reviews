f506bf20a7b58d77fc0dd1a0cd3c3a4dd9fc9d30
Alex Carlman <alexcarlman1@gmail.com>
2021-11-29 16:23:46 -0500

    Separate db and server

--- database/queries.js
+++ database/queries.js
@@ -3,18 +3,175 @@ const db = pgp('postgres://postgres:postgres@localhost:5432/reviews')
 
 const Queries = {};
 
-Queries.getAll = (id, sort, count) => {
+Queries.getAll = (id, page, count, offset, sort) => {
   return db.any(`SELECT *, COALESCE(photos.ph, '[]') AS photos
   FROM reviews r
-  LEFT JOIN LATERAL(
+  INNER JOIN LATERAL(
     SELECT json_agg(json_build_object('id', photos.id, 'url', photos.url))
     AS ph
     FROM photos
     WHERE photos.review_id=r.review_id) photos ON true
     WHERE r.product_id=$1
     AND r.reported=false
-    ORDER BY $2 DESC
-    LIMIT $3 ;`, [id, sort, count],false);
+    OFFSET $2
+    LIMIT $3 ;`, [id, offset, count])
+    .then((data) => {
+      var results = [];
+      var header = {
+        "product" : id,
+        "page": page,
+        "count": count,
+        "results": []
+      }
+      data.map((review, i) => {
+        if (i <= count) {
+          var result = {
+            "review_id": review.review_id,
+            "rating": review.rating,
+            "summary": review.summary,
+            "recommend": review.recommend,
+            "response": review.response,
+            "body": review.body,
+            "date": review.review_date,
+            "reviewer_name": review.reviewer_name,
+            "helpfulness": review.helpfulness,
+            "photos": review.photos || []
+          }
+          header.results.push(result);
+        }
+      })
+      return header;
+    })
+    .catch((err) => {
+      return err;
+    })
+}
+
+Queries.getRatings = (id) => {
+  return db.any(`SELECT
+  json_build_object(
+  1, (SELECT
+    COUNT(reviews.rating)
+    FROM reviews
+    WHERE reviews.product_id= $1 AND reviews.rating = 1),
+  2, (SELECT
+    COUNT(reviews.rating)
+    FROM reviews
+    WHERE reviews.product_id= $1 AND reviews.rating = 2),
+  3, (SELECT
+    COUNT(reviews.rating)
+    FROM reviews
+    WHERE reviews.product_id= $1 AND reviews.rating = 3),
+  4, (SELECT
+    COUNT(reviews.rating)
+    FROM reviews
+    WHERE reviews.product_id= $1 AND reviews.rating = 4),
+  5, (SELECT
+    COUNT(reviews.rating)
+    FROM reviews
+    WHERE reviews.product_id= $1 AND reviews.rating = 5)
+  )`, id)
+    .then((data) => {
+      return data[0]["json_build_object"];
+    })
+    .catch((err) => {
+      return err;
+    })
+}
+
+Queries.getRecommended = (id) => {
+  return db.any(`SELECT
+      json_build_object(
+      0, (SELECT
+        COUNT(reviews.recommend)
+        FROM reviews
+        WHERE reviews.product_id=$1 AND reviews.recommend=false),
+      1, (SELECT
+        COUNT(reviews.recommend)
+        FROM reviews
+        WHERE reviews.product_id=$1 AND reviews.recommend=true))`, id)
+      .then((result) => {
+       return result[0]["json_build_object"];
+      })
+      .catch((err) => {
+        return err;
+      })
+}
+
+Queries.getCharacteristics = (id) => {
+  return db.any(`SELECT
+        json_build_object(
+        characteristics.name,
+        json_agg(json_build_object(
+          'id', characteristics.id,
+          'value', (SELECT AVG (CAST(characteristic_reviews.value as Float))
+          FROM characteristic_reviews
+          WHERE characteristic_reviews.characteristic_id = characteristics.id
+          ))
+          )
+        ) characteristics FROM characteristics WHERE characteristics.product_id=$1
+       GROUP BY characteristics.name`, id)
+        .then((data) => {
+          var chars = {};
+          data.map((char) => {
+            //console.log(char)
+            var type = Object.keys(char.characteristics)[0];
+            chars[type] = char.characteristics[type][0];
+          })
+          return chars;
+        })
+        .catch((err) => {
+          return err;
+        })
+}
+
+Queries.postReview = (product_id, rating, summary, body, recommend, name, email, reported, date) => {
+  return db.any(`INSERT INTO reviews (product_id, rating, summary, body, recommend, reviewer_name, reviewer_email, reported, review_date) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)`, [product_id, rating, summary, body, recommend, name, email, reported, date])
+  .then((data) => 'success')
+  .catch((e) => e);
+}
+
+Queries.postCharacteristics = (characteristics) => {
+  return db.any('SELECT MAX(review_id) FROM reviews')
+    .then ((review_id) => {
+      Object.keys(characteristics).map((key, index) => {
+        db.any('INSERT INTO characteristic_reviews (characteristic_id, review_id, value) VALUES ($1, $2, $3)', [key, review_id[0].max, characteristics[key]])
+      })
+      return 'success';
+    })
+    .catch((e) => e);
+}
+
+Queries.postPhotos = (photos) => {
+  return db.any('SELECT MAX(review_id) FROM reviews')
+  .then ((review_id) => {
+    photos.map((photo) => {
+    db.any('INSERT INTO photos (review_id, url) VALUES ($1, $2)', [review_id[0].max, photo])
+    .then((result) => 'success')
+    .catch((e) => e)
+  })
+  })
+  .catch((e) => e);
+}
+
+Queries.putHelpful = (id) => {
+  return db.any('UPDATE reviews SET helpfulness = helpfulness + 1 WHERE review_id=$1;', id)
+  .then((data) => {
+    return 'success';
+  })
+  .catch((err) => {
+    return 'error';
+  })
+}
+
+Queries.putReported = (id) => {
+  return db.any('UPDATE reviews SET reported = true WHERE review_id=$1', id)
+    .then((data) => {
+      return 'success'
+    })
+    .catch((err) => {
+      return err
+    })
 }
 
 module.exports = Queries;
--- server/index.js
+++ server/index.js
@@ -4,9 +4,7 @@ const app = express();
 const path = require('path');
 const port = 3000;
 
-// Database
-const pgp = require('pg-promise')(/* options */)
-const db = pgp('postgres://postgres:postgres@localhost:5432/reviews')
+const queries = require('../database/queries.js');
 
 app.use(express.json());
 app.use('/', express.static(path.join(__dirname, '../dist')));
@@ -19,131 +17,32 @@ app.get('/reviews', (req, res) => {
   var id = Number(req.query.product_id);
   var page = req.query.page || 1;
   var count = req.query.count || 5;
-  console.log(count);
   var offset = (page - 1) * count;
   var sort = req.query.sort || 'review_date';
-  db.any(`SELECT *, COALESCE(photos.ph, '[]') AS photos
-  FROM reviews r
-  INNER JOIN LATERAL(
-    SELECT json_agg(json_build_object('id', photos.id, 'url', photos.url))
-    AS ph
-    FROM photos
-    WHERE photos.review_id=r.review_id) photos ON true
-    WHERE r.product_id=$1
-    AND r.reported=false
-    OFFSET $2
-    LIMIT $3 ;`, [id, offset, count])
-    .then((data) => {
-      console.log(data)
-      var results = [];
-      var header = {
-        "product" : id,
-        "page": page,
-        "count": count,
-        "results": []
-      }
-      data.map((review, i) => {
-        if (i <= count) {
-          var result = {
-            "review_id": review.review_id,
-            "rating": review.rating,
-            "summary": review.summary,
-            "recommend": review.recommend,
-            "response": review.response,
-            "body": review.body,
-            "date": review.review_date,
-            "reviewer_name": review.reviewer_name,
-            "helpfulness": review.helpfulness,
-            "photos": review.photos || []
-          }
-          header.results.push(result);
-        }
-      })
-      return header;
-    })
-    .then((header) => {
-      res.status(200).send(header);
-    })
-    .catch((err) => {
-      console.log('ERR:', err)
-      res.status(500).send(err);
-    })
+  queries.getAll(id, page, count, offset, sort)
+  .then((result) => res.status(200).send(result))
+  .catch((e) => res.status(500).send(err));
 })
 
 app.get('/reviews/meta', (req, res) => {
   var id = Number(req.query.product_id);
-  console.log(id);
   var response = {
     "product_id": id
   }
-db.any(`SELECT
-json_build_object(
-  1, (SELECT
-    COUNT(reviews.rating)
-    FROM reviews
-    WHERE reviews.product_id= $1 AND reviews.rating = 1),
-  2, (SELECT
-    COUNT(reviews.rating)
-    FROM reviews
-    WHERE reviews.product_id= $1 AND reviews.rating = 2),
-  3, (SELECT
-    COUNT(reviews.rating)
-    FROM reviews
-    WHERE reviews.product_id= $1 AND reviews.rating = 3),
-  4, (SELECT
-    COUNT(reviews.rating)
-    FROM reviews
-    WHERE reviews.product_id= $1 AND reviews.rating = 4),
-  5, (SELECT
-    COUNT(reviews.rating)
-    FROM reviews
-    WHERE reviews.product_id= $1 AND reviews.rating = 5)
-  )`, id)
-    .then((data) => {
-      response.ratings= data[0]["json_build_object"];
-      db.any(`SELECT
-      json_build_object(
-      0, (SELECT
-        COUNT(reviews.recommend)
-        FROM reviews
-        WHERE reviews.product_id=$1 AND reviews.recommend=false),
-      1, (SELECT
-        COUNT(reviews.recommend)
-        FROM reviews
-        WHERE reviews.product_id=$1 AND reviews.recommend=true))`, id)
-      .then((result) => {
-       response.recommended = result[0]["json_build_object"];
-        db.any(`SELECT
-        json_build_object(
-        characteristics.name,
-        json_agg(json_build_object(
-          'id', characteristics.id,
-          'value', (SELECT AVG (CAST(characteristic_reviews.value as Float))
-          FROM characteristic_reviews
-          WHERE characteristic_reviews.characteristic_id = characteristics.id
-          ))
-          )
-        ) characteristics FROM characteristics WHERE characteristics.product_id=$1
-       GROUP BY characteristics.name`, id)
-        .then((data) => {
-          console.log(data[0].characteristics);
-          var chars = {};
-          data.map((char) => {
-            //console.log(char)
-            var type = Object.keys(char.characteristics)[0];
-            chars[type] = char.characteristics[type][0];
-          })
-          response.characteristics = chars;
-          res.status(200).send(response);
-        })
-        .catch((err) => {
-          console.log(err);
-          res.status(500).send();
-        })
+  queries.getRatings(id)
+  .then((ratings) => {
+    response.ratings = ratings;
+    queries.getRecommended(id)
+    .then((recommended) => {
+      response.recommended = recommended;
+      queries.getCharacteristics(id)
+      .then((characteristics) => {
+        response.characteristics = characteristics;
+        res.status(200).send(response);
       })
-    }
-    )
-
+    })
+  })
+  .catch((err) => res.status(500).send(err));
 
 })
 
@@ -152,26 +51,14 @@ app.post('/reviews', (req, res) => {
   var d = new Date();
   var timeZoneOffset = d.getTimezoneOffset() * 60000;
   var date = new Date(d.getTime() - timeZoneOffset);
-  db.any(`INSERT INTO reviews (product_id, rating, summary, body, recommend, reviewer_name, reviewer_email, reported, review_date) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)`, [req.body.product_id, req.body.rating, req.body.summary, req.body.body, req.body.recommend, req.body.name, req.body.email, false, date])
+  queries.postReview(req.body.product_id, req.body.rating, req.body.summary, req.body.body, req.body.recommend, req.body.name, req.body.email, false, date)
   .then((result) => {
-    db.any('SELECT MAX(review_id) FROM reviews')
-    .then ((review_id) => {
-      reviewId=review_id[0].max;
-      Object.keys(req.body.characteristics).map((key, index) => {
-        console.log('KEY > ', key);
-        console.log('VALUE > ', req.body.characteristics[key])
-        db.any('INSERT INTO characteristic_reviews (characteristic_id, review_id, value) VALUES ($1, $2, $3)', [key, reviewId, req.body.characteristics[key]])
-      })
-    })
+    queries.postCharacteristics(req.body.characteristics)
     .then((data) => {
-      console.log('.then ran');
-      req.body.photos.map((photo, i) => {
-        console.log('map ran x', i);
-        db.any('INSERT INTO photos (review_id, url) VALUES ($1, $2)', [reviewId, photo])
+      queries.postPhotos(req.body.photos)
+      .then((data) => {
+        res.status(201).send('CREATED')
       })
-  })
-  .then((result) => {
-    res.status(201).send('CREATED');
   })
   .catch((err) => {
     console.log(err);
@@ -182,22 +69,22 @@ app.post('/reviews', (req, res) => {
 
 app.put('/reviews/:review_id/helpful', (req, res) => {
   var id = req.params.review_id;
-  db.any('UPDATE reviews SET helpfulness = helpfulness + 1 WHERE review_id=$1;', id)
-    .then((data) => {
-      res.status(204).send();
-    })
-    .catch((err) => {
-      res.status(500).send();
-    })
+  queries.putHelpful(id)
+  .then((result) => {
+    res.status(204).send();
+  })
+  .catch((err) => {
+    res.status(500).send();
+  })
 })
 
 app.put('/reviews/:review_id/report', (req, res) => {
   var id = req.params.review_id;
-  db.any('UPDATE reviews SET reported = true WHERE review_id=$1', id)
-    .then((data) => {
-      res.status(204).send();
-    })
-    .catch((data) => {
-      res.status(500).send();
-    })
+  queries.putReported(id)
+  .then((result) => {
+    res.status(204).send();
+  })
+  .catch((err) => {
+    res.status(500).send();
+  })
 })
